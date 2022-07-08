90e3020d22268245f10eb021f6919effa05b944a
Kirisu <56469224+rubenkristian@users.noreply.github.com>
2022-07-08 11:08:16 +0700

    feat: move load secret to module and update test (#1)
    * move load secret to module and update test
    
    * fix: shorthand linter error
    
    * feat: new beta version
    
    Co-authored-by: Agustinus Theodorus <agustinustheoo@gmail.com>

--- lib/gcloud-secret-manager.service.ts
+++ lib/gcloud-secret-manager.service.ts
@@ -3,49 +3,14 @@ import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 
 @Injectable()
 export class GCloudSecretManagerService {
-  private readonly logger: Logger = new Logger(GCloudSecretManagerService.name);
-  private _client: SecretManagerServiceClient;
-  private _readConfig: boolean = false;
+  private _secretsList: Map<string, string>;
 
-  private _parentSecrets: string;
-  private _secretsList: Map<string, string> = new Map<string, string>();
-  private _secretsLoaded: boolean = false;
-
-  constructor(parent: string) {
-    this._parentSecrets = parent;
-    this._client = new SecretManagerServiceClient();
-  }
-
-  async loadSecrets() {
-    if (this._readConfig) return;
-    try {
-      this._readConfig = true;
-
-      const [secrets] = await this._client.listSecrets({
-        parent: this._parentSecrets,
-      });
-
-      for (const secret of secrets) {
-        const arrPath = secret.name.split('/');
-        const envId = arrPath.slice(-1)[0];
-        const [version] = await this._client.accessSecretVersion({
-          name: `${secret.name}/versions/latest`,
-        });
-        const value = version.payload.data.toString();
-        this._secretsList.set(envId, value);
-      }
-
-      this._secretsLoaded = true;
-    } catch (err) {
-      this.logger.log(err);
-      throw new Error(err);
-    } finally {
-      this._readConfig = false;
-    }
+  constructor(secretsList: Map<string, string>) {
+    this._secretsList = secretsList;
   }
 
   getSecret(key: string): String {
-    if (!this._secretsLoaded) throw new Error('Secrets are not loaded');
+    if (this._secretsList.size <= 0) throw new Error('Secrets are not loaded');
     return this._secretsList.get(key);
   }
 }
