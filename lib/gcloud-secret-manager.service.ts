30c3f2a1133132d42001bdd8a552ec42c49d36fc
Kirisu <56469224+rubenkristian@users.noreply.github.com>
2022-07-08 14:05:23 +0700

    refactor: package and set module to global (#2)
    * refactor package and set module Global
    
    * feat: update beta release
    
    Co-authored-by: Agustinus Theodorus <agustinustheoo@gmail.com>

--- lib/gcloud-secret-manager.service.ts
+++ lib/gcloud-secret-manager.service.ts
@@ -3,10 +3,40 @@ import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 
 @Injectable()
 export class GCloudSecretManagerService {
+  private static _gCloudSecretManagerService: GCloudSecretManagerService;
   private _secretsList: Map<string, string>;
 
   constructor(secretsList: Map<string, string>) {
     this._secretsList = secretsList;
+  }
+
+  static async loadSecrets(parent: string) {
+    if (!this._gCloudSecretManagerService) {
+      const secretsList: Map<string, string> = new Map<string, string>();
+      const client: SecretManagerServiceClient = new SecretManagerServiceClient();
+
+      try {
+        const [secrets] = await client.listSecrets({
+          parent,
+        });
+
+        for (const secret of secrets) {
+          const arrPath = secret.name.split('/');
+          const envId = arrPath.slice(-1)[0];
+          const [version] = await client.accessSecretVersion({
+            name: `${secret.name}/versions/latest`,
+          });
+          const value = version.payload.data.toString();
+          secretsList.set(envId, value);
+        }
+
+        this._gCloudSecretManagerService = new GCloudSecretManagerService(secretsList);
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+
+    return this._gCloudSecretManagerService;
   }
 
   getSecret(key: string): String {
