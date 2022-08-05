7832c20087075cd07c306711d8e6ffb7a9205166
rubenkristian <rubenkristian96@gmail.com>
2022-08-05 22:09:42 +0700

    - using generic classes for google secret manager service - interface for key list - insert value to secret list if key exists - set value from google secret manager if value from key list parameter is null or value from key list if not null or string

--- lib/gcloud-secret-manager.module.ts
+++ lib/gcloud-secret-manager.module.ts
@@ -1,6 +1,7 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
-import { GCLOUD_SECRET_MANAGER_PARENT } from './gcloud-secret-manager.constant';
+import { GCLOUD_SECRET_KEY_AND_VALUE, GCLOUD_SECRET_MANAGER_PARENT } from './gcloud-secret-manager.constant';
 import { GCloudSecretManagerService } from './gcloud-secret-manager.service';
+import { GoogleCloudKeyValue } from './interface/gcloud-key-value.interface';
 
 @Global()
 @Module({
@@ -8,21 +9,27 @@ import { GCloudSecretManagerService } from './gcloud-secret-manager.service';
   exports: [GCloudSecretManagerService],
 })
 export class GCloudSecretManagerModule {
-  static async withConfig(parent: string): Promise<DynamicModule> {
+  static async withConfig(parent: string, listKeyValue: GoogleCloudKeyValue): Promise<DynamicModule> {
     const secretListModuleOptions = {
       provide: GCLOUD_SECRET_MANAGER_PARENT,
       useValue: parent,
     };
 
+    const googleCloudKeyValue = {
+      provide: GCLOUD_SECRET_KEY_AND_VALUE,
+      useValue: listKeyValue,
+    };
+
     const gsmServiceProvider = {
       provide: GCloudSecretManagerService,
-      useFactory: async (_parent: string) => await GCloudSecretManagerService.loadSecrets(_parent),
-      inject: [GCLOUD_SECRET_MANAGER_PARENT],
+      useFactory: async (_parent: string, _listKeyValue: GoogleCloudKeyValue) =>
+        await GCloudSecretManagerService.loadSecrets(_parent, _listKeyValue),
+      inject: [GCLOUD_SECRET_MANAGER_PARENT, GCLOUD_SECRET_KEY_AND_VALUE],
     };
 
     return {
       module: GCloudSecretManagerModule,
-      providers: [gsmServiceProvider, secretListModuleOptions],
+      providers: [gsmServiceProvider, secretListModuleOptions, googleCloudKeyValue],
       exports: [GCloudSecretManagerService],
     };
   }
--- lib/gcloud-secret-manager.service.ts
+++ lib/gcloud-secret-manager.service.ts
@@ -1,16 +1,17 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
+import { GoogleCloudKeyValue } from './interface/gcloud-key-value.interface';
 
 @Injectable()
-export class GCloudSecretManagerService {
-  private static _gCloudSecretManagerService: GCloudSecretManagerService;
-  private _secretsList: Map<string, string>;
+export class GCloudSecretManagerService<T> {
+  private static _gCloudSecretManagerService: GCloudSecretManagerService<any>;
+  private _secretsList: Map<T, string>;
 
-  constructor(secretsList: Map<string, string>) {
+  constructor(secretsList: Map<T, string>) {
     this._secretsList = secretsList;
   }
 
-  static async loadSecrets(parent: string) {
+  static async loadSecrets(parent: string, listKeyValue: GoogleCloudKeyValue) {
     if (!this._gCloudSecretManagerService) {
       const secretsList: Map<string, string> = new Map<string, string>();
       const client: SecretManagerServiceClient = new SecretManagerServiceClient();
@@ -23,11 +24,16 @@ export class GCloudSecretManagerService {
         for (const secret of secrets) {
           const arrPath = secret.name.split('/');
           const envId = arrPath.slice(-1)[0];
-          const [version] = await client.accessSecretVersion({
-            name: `${secret.name}/versions/latest`,
-          });
-          const value = version.payload.data.toString();
-          secretsList.set(envId, value);
+
+          const valueOfKey = listKeyValue[envId];
+
+          if (valueOfKey !== undefined) {
+            const [version] = await client.accessSecretVersion({
+              name: `${secret.name}/versions/latest`,
+            });
+            const value = version.payload.data.toString();
+            secretsList.set(envId, valueOfKey ?? value);
+          }
         }
 
         this._gCloudSecretManagerService = new GCloudSecretManagerService(secretsList);
@@ -39,7 +45,7 @@ export class GCloudSecretManagerService {
     return this._gCloudSecretManagerService;
   }
 
-  getSecret(key: string): String {
+  getSecret(key: T): String {
     if (this._secretsList.size <= 0) throw new Error('Secrets are not loaded');
     return this._secretsList.get(key);
   }
